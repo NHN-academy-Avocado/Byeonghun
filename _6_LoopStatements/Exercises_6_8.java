3713cfb8326a2a7fa2c8733b2813c1e04b92487a
정병훈 <rootofproblem23@gmail.com>
2023-09-20 09:23:27 +0900

    6장 끝

--- _6_LoopStatements/Exercises_6_8.java
+++ _6_LoopStatements/Exercises_6_8.java
@@ -2,11 +2,23 @@ package _6_LoopStatements;
 
 public class Exercises_6_8 {
     public static void main(String[] args) {
-        for (int i = 1; i < 20; i++) {
-            for (int j = 2; j < 20; j++) {
-                System.out.print(i*j + "\t");
+        gugudan(20,20);
+    }
+
+    private static void gugudan(int a, int b) {
+        for (int i = 1; i <= a; i++) {
+            for (int j = 2; j <= b; j++) {
+                multple(i, j);
             }
-            System.out.println();
+            getPrintln();
         }
+    }
+
+    private static void getPrintln() {
+        System.out.println();
+    }
+
+    private static void multple(int i, int j) {
+        System.out.print(i * j + "\t");
     }
 }
