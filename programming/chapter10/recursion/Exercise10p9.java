370925049243874e26403bfe74b66490e6cc6b0e
정병훈 <rootofproblem23@gmail.com>
2023-09-26 10:10:34 +0900

    10장 9번까지

--- programming/chapter10/recursion/Exercise10p9.java
+++ programming/chapter10/recursion/Exercise10p9.java
@@ -2,20 +2,20 @@ package programming.chapter10.recursion;
 
 import java.util.Arrays;
 
-public class Exercise10p7 {
+public class Exercise10p9 {
     public static void main(String[] args) {
-        Moor m = new Moor(5, 5, 0.5);
-        Traversal t = new Traversal(m);
+        Moor8 m = new Moor8(5, 5, 0.5);
+        Traversal8 t = new Traversal8(m);
         t.search();
         System.out.println(t);
     }
 }
 
-class Moor {
+class Moor8 {
 
     private boolean[][] moor;
 
-    public Moor(int rows, int columns, double probLand) {
+    public Moor8(int rows, int columns, double probLand) {
         moor = new boolean[rows][columns];
         for (int r = 0; r < rows; r++)
             for (int c = 0; c < columns; c++)
@@ -48,22 +48,22 @@ class Moor {
 }
 
 
-class Traversal {
+class Traversal8 {
 
     private int[] traversal;
-    private Moor moor;
+    private Moor8 moor;
     private boolean found;
     private boolean[][] visited;
 
-    public Traversal(Moor m) {
+    public Traversal8(Moor8 m) {
         moor = m;
         traversal = new int[moor.getNumColumns()];
         Arrays.fill(traversal, -1);
         visited = new boolean[moor.getNumRows()][moor.getNumColumns()];
-        found = traverseMoor(moor, traversal);
+        found = traverseMoor(moor, traversal, visited);
     }
 
-    public Moor moor() {
+    public Moor8 moor() {
         return moor;
     }
 
@@ -89,21 +89,19 @@ class Traversal {
         StringBuilder str = new StringBuilder();
         for (int i = 0; i < moor.getNumRows(); i++) {
             for (int j = 0; j < moor().getNumColumns(); j++) {
-                if (moor.land(i, j)){
+                if (moor.land(i, j)) {
                     if (traversal[j] == i) str.append("# ");
                     else str.append("* ");
-                }
-                else str.append("O ");
+                } else str.append("O ");
             }
             str.append("\n");
         }
         str.append("===================== \n");
         for (int i = 0; i < moor.getNumRows(); i++) {
             for (int j = 0; j < moor().getNumColumns(); j++) {
-                if (moor.land(i, j)){
+                if (moor.land(i, j)) {
                     str.append("* ");
-                }
-                else str.append("O ");
+                } else str.append("O ");
             }
             str.append("\n");
         }
@@ -113,15 +111,20 @@ class Traversal {
 
     // auxiliary methods
 
-    private static boolean traverseMoor(Moor m, int[] path) {
-        for (int row = 0; row < m.getNumRows(); row++) {
-            if (searchTraversal(m, row, 0, path)) return true;
-        }
+    private static boolean traverseMoor(Moor8 m, int[] path) {
+
+        if (searchTraversal(m, m.getNumRows() / 2, m.getNumColumns() / 2, path)) return true;
+
+        return false;
+    }
+    private static boolean traverseMoor(Moor8 m, int[] path, boolean[][] visited) {
+
+        if (searchTraversal(m, m.getNumRows() / 2, m.getNumColumns() / 2, path, visited)) return true;
 
         return false;
     }
 
-        private static boolean searchTraversal(Moor m, int r, int c, int[] path) {
+    private static boolean searchTraversal(Moor8 m, int r, int c, int[] path) {
         if (!m.land(r, c))
             return false;
         else {
@@ -134,19 +137,26 @@ class Traversal {
                         searchTraversal(m, r + 1, c + 1, path);
         }
     }
-    private static boolean searchTraversal(Moor m, int r, int c, int[] path, boolean[][] visited) {
+
+    private static boolean searchTraversal(Moor8 m, int r, int c, int[] path, boolean[][] visited) {
         if (!m.land(r, c) || visited[r][c])
             return false;
 
         visited[r][c] = true;
         path[c] = r;
 
-        if (c == m.getNumColumns() - 1)
+        if (c == m.getNumColumns() - 1 || c == 0 || r == 0 || r == m.getNumRows())
             return true;
 
-        return searchTraversal(m, r - 1, c + 1, path, visited) ||
-                searchTraversal(m, r, c + 1, path, visited) ||
-                searchTraversal(m, r + 1, c + 1, path, visited);
+        return searchTraversal(m, r + 1, c - 1, path, visited) ||
+                searchTraversal(m, r + 0, c - 1, path, visited) ||
+                searchTraversal(m, r - 1, c - 1, path, visited) ||
+                searchTraversal(m, r - 1, c + 0, path, visited) ||
+                searchTraversal(m, r + 1, c + 0, path, visited) ||
+                searchTraversal(m, r - 1, c + 1, path, visited) ||
+                searchTraversal(m, r + 0, c + 1, path, visited) ||
+                searchTraversal(m, r + 1, c + 1, path, visited)
+                ;
     }
 
     public void search() {
